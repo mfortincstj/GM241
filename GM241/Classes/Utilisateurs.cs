f0dea025841355700b0893c66ae522503768cad9
mfortincstj <1109047@cstj.qc.ca>
2014-10-02 19:33:06 -0400

    Structure + Login
    Création du projet dans Visual Studio + création de la base de fenêtre
    login

---
+++ GM241/Classes/Utilisateurs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GM241.Classes
+{
+    class Utilisateurs
+    {
+        #region
+            public virtual int? matricule { get; set; }
+            public virtual string prenom { get; set; }
+            public virtual string nom { get; set; }
+            private string motDePasse { get; set; }
+            public virtual bool estAdmin { get; set; }
+        #endregion
+    }
+}
---
+++ GM241/Fenetres/MenuPrincipal/MenuPrincipal.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GM241.Fenetres.MenuPrincipal
+{
+    /// <summary>
+    /// Logique d'interaction pour MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
