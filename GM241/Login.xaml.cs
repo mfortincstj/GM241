9a5e192febf1848cb022a4f44e2c8efd1edce6cd
mfortincstj <1109047@cstj.qc.ca>
2014-10-06 13:46:24 -0400

    Login - MenuPrincipal - En cours
    Relier les 2 pages ensembles - En cours

--- GM241/Login.xaml.cs
+++ GM241/Login.xaml.cs
@@ -25,8 +25,29 @@ namespace GM241
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Clic sur le bouton authentification
+        private void authentification_Click(object sender, RoutedEventArgs e)
         {
+            // Valider le nom d'utilisatuer en BD
+            string usagerRecu = usager.Text;
+
+            // Voir en BD si ce nom d'usager est valide ?
+
+
+            // Valider le mot de passe en BD
+            string motDePasseRecu = motDePasse.Text;
+
+            // Si tout est valide, on passe au menu principal
+            Login pageLogin = new Login();
+            pageLogin.Close();
+
+
+        }
+
+        // Clic sur quitter
+        private void quitter_Click(object sender, RoutedEventArgs e)
+        {
+            // Fermeture du projet
             Application.Current.Shutdown();
         }
     }
