8bc9182bf4592631cbc6da3dac26f542ff615457
mfortincstj <1109047@cstj.qc.ca>
2014-10-20 13:37:17 -0400

    ajoutAttachement

---
+++ GM241/Fenetres/Produit/ajoutAttachement.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GM241.Fenetres.Produit
+{
+    /// <summary>
+    /// Logique d'interaction pour ajoutAttachement.xaml
+    /// </summary>
+    public partial class ajoutAttachement : Window
+    {
+        public ajoutAttachement()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFermer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
