ec3c0be7ad115a61b6b476728689ea02f1c15f90
Mynoky <noki1992@gmail.com>
2014-10-23 14:58:50 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	GM241/GM241.v12.suo

--- GM241/Classes/RequetesBD.cs
+++ GM241/Classes/RequetesBD.cs
@@ -9,6 +9,30 @@ namespace GM241.Classes
 {
     class RequetesBD
     {
-        
+        public List<String>[] function_SelectOutils()
+        {
+
+            BDService BDoutils = new BDService();
+            String request = "SELECT * FROM Outils";
+
+                List<string>[] tabOut;
+                int nombreRange = 0;
+                tabOut = BDoutils.selection(request, 5, ref nombreRange);
+
+            return tabOut;      
+        }
+
+        public List<String>[] function_SelectCollets()
+        {
+
+            BDService BDCollets = new BDService();
+            String request = "SELECT * FROM Collets";
+
+                List<string>[] tabCol;
+                int nombreRange = 0;
+                tabCol = BDCollets.selection(request, 7, ref nombreRange);
+
+            return tabCol;
+        }
     }
 }
