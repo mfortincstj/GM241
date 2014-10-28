18b26780bbd765ac11d385b1ab8dd28b0f32683d
mfortincstj <1109047@cstj.qc.ca>
2014-10-28 11:24:11 -0400

    a
    a

--- GM241/Fenetres/Menu/MenuPrincipal.xaml.cs
+++ GM241/Fenetres/Menu/MenuPrincipal.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using GM241.Fenetres.InventaireGestion;
+using GM241.Fenetres.MenuPrincipal;
 
 namespace GM241.Fenetres.MenuPrincipal
 {
