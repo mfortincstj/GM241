b117d16db50d6ad6fdd2ec1e9c485715679b6052
mfortincstj <1109047@cstj.qc.ca>
2014-11-19 11:19:52 -0500

    Création de classes et fenetres

---
+++ GM241/Fenetres/Produit/Machine.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GM241.Fenetres.Produit
+{
+    /// <summary>
+    /// Logique d'interaction pour Machine.xaml
+    /// </summary>
+    public partial class Machine : Window
+    {
+        public Machine()
+        {
+            InitializeComponent();
+        }
+
+        private void axeZOui_Checked(object sender, RoutedEventArgs e)
+        {
+            boxAxeZMin.IsEnabled = true;
+            boxAxeZMax.IsEnabled = true;
+        }
+
+        private void axeZNon_Checked(object sender, RoutedEventArgs e)
+        {
+            boxAxeZMin.IsEnabled = false;
+            boxAxeZMax.IsEnabled = false;
+        }
+    }
+}
